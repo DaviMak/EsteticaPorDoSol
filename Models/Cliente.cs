ba12e8c66b3b9ba06dcf0e84399ae54f28a7f91c
DaviMak <davi.amak@gmail.com>
2026-06-03 01:52:37 -0300

    historico, validacoes, busca por placa e nome(parcial)e fluxo de cadastro em andamento

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -6,6 +6,7 @@ namespace EsteticaPorDoSol.Models
     {
         [Key]
         public int idCliente { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
         public required string dsNome { get; set; }
         public string? nrTelefone { get; set; }
         public List<Veiculo> Veiculos { get; set; } = new();
--- Models/Servico.cs
+++ Models/Servico.cs
@@ -6,8 +6,10 @@ namespace EsteticaPorDoSol.Models
     {
         [Key]
         public int idServico { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
         public required string dsNomeServico { get; set; }
         public required string dsDescricaoServico { get; set; }
         public decimal vlServico { get; set; }
+        
     }
 }
