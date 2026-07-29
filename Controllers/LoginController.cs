f73b1b7a99cc88269d6009fe55e17655c998afa7
DaviMak <davi.amak@gmail.com>
2026-07-28 21:34:54 -0300

    tela de login simples

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EsteticaPorDoSol.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult TelaLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(string usuario, string senha)
+        {
+            if (usuario == "kelvin" && senha == "12345")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            TempData["Erro"] = "Usuário ou senha incorretos.";
+            return RedirectToAction("TelaLogin");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=TelaLogin}/{id?}");
 
 app.Run();
